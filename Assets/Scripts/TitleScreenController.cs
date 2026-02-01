ec77a44896dcd6ec564a44142e6db950df03a554
Jordan Eggleston <JordanH.Eggleston@gmail.com>
2026-02-01 07:43:44 -0500

    Double Scenage

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TitleScreenController : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+}
